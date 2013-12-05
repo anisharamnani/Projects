fc88135715f64e4328aeb60b3265997f8f065bcc
anisharamnani <anisha.ramnani@gmail.com>
2013-12-04 23:45:14 -0500

    business hours updatez

--- Business_Hours/business_hours_spec.rb
+++ Business_Hours/business_hours_spec.rb
@@ -1,44 +1,58 @@
 require './lib/Business_Hours'
 
-describe Business_Hours, "update_all" do
+describe Business_Hours do
+	subject{Business_Hours.new("09:00 AM","03:00 PM")}
+	expected = {
+							:sun => {:open => "09:00 AM", :close => "03:00 PM", :opened => true},
+							:mon => {:open => "09:00 AM", :close => "03:00 PM", :opened => true}, 
+							:tue => {:open => "09:00 AM", :close => "03:00 PM", :opened => true}, 
+							:wed => {:open => "09:00 AM", :close => "03:00 PM", :opened => true}, 
+							:thu => {:open => "09:00 AM", :close => "03:00 PM", :opened => true}, 
+							:fri => {:open => "09:00 AM", :close => "03:00 PM", :opened => true}, 
+							:sat =>{:open => "09:00 AM", :close => "03:00 PM", :opened => true}
+							} 
 
-	context "utility methods" do 
-		subject{Business_Hours.new("9:00 AM","3:00 PM")}
-		expected = {
-								:sun => {:open => "9:00 AM", :close => "3:00 PM"},
-								:mon => {:open => "9:00 AM", :close => "3:00 PM"}, 
-								:tue => {:open => "9:00 AM", :close => "3:00 PM"}, 
-								:wed => {:open => "9:00 AM", :close => "3:00 PM"}, 
-								:thurs => {:open => "9:00 AM", :close => "3:00 PM"}, 
-								:fri => {:open => "9:00 AM", :close => "3:00 PM"}, 
-								:sat =>{:open => "9:00 AM", :close => "3:00 PM"}
-								} 
 
-		it "should take the paramaters for open and close and update the hash" do 
+	describe "#update_all" do 
+		it "#subject_all should take the paramaters for open and close and update the hash" do 
 			expect(subject.update_all).to eq(expected)
 		end
+	end 
 
+	describe "update_one" do 
 		it "should take the paramaters for open and close and update the hash" do
 			subject.hours = expected
-
-			expect(subject.update_one(:fri, "11:00 AM", "4:00 PM")).to eq(
-				{
-				:sun => {:open => "9:00 AM", :close => "3:00 PM"},
-				:mon => {:open => "9:00 AM", :close => "3:00 PM"}, 
-				:tue => {:open => "9:00 AM", :close => "3:00 PM"}, 
-				:wed => {:open => "9:00 AM", :close => "3:00 PM"}, 
-				:thurs => {:open => "9:00 AM", :close => "3:00 PM"}, 
-				:fri => {:open => "11:00 AM", :close => "4:00 PM"}, 
-				:sat =>{:open => "9:00 AM", :close => "3:00 PM"}
-				})
+			subject.update_one("fri", "11:00 AM", "04:00 PM")
+			expect(subject.hours[:fri][:open]).to eq("11:00 AM")
+			expect(subject.hours[:fri][:close]).to eq("04:00 PM")
 		end 
+	end 
 
+	describe "#calculate_deadline" do 
 		it "should calculate the deadline given a date and deadline time" do
-			expect(subject.calculate_deadline(2*60*60, "Jun 7, 2010 9:10 AM")).to
-			eq("Jun 07 11:10:00")
+			expect(subject.calculate_deadline(2*60*60, "Jun 7, 2010 09:10 AM")).to eq("Mon Jun 07 11:10:00 2010")
 		end
-	
+	end 
 
+	describe "#close" do 
+		it "should set a day to closed" do 
+			subject.hours = expected
+			subject.close("mon")
+			expect(subject.hours[:mon][:opened]).to eq(false)
+		end 
+	end 
+
+	describe "#closed?" do
+		it "should return the closed days" do 
+			subject.hours = expected
+			subject.close("mon") 
+			expect(subject.closed?).to eq(["mon"])
+		end 
+	end 
+
+	describe "#compare" do 
+		it "should compare the return time to the hours" do 
+		end 
 	end 
 
 end 
--- Business_Hours/lib/Business_Hours.rb
+++ Business_Hours/lib/Business_Hours.rb
@@ -10,35 +10,63 @@ class Business_Hours
 		@hours = {
 							:sun => {},
 							:mon => {}, :tue => {}, :wed => {}, 
-							:thurs => {}, :fri => {}, :sat =>{}
+							:thu => {}, :fri => {}, :sat =>{} 
 							}	
 
 	end 
 
-	# write a method that makes string values into days 
-	# def date_to_day 
-	# end 
-
 	def update_all
 		hours.each_pair {|day, times| times[:open] = @open}
 		hours.each_pair {|day, times| times[:close] = @close}
+		hours.each_pair {|day, times| times[:opened] = true}
 		hours
 	end 
 
-	# change the opening and closing time for a given day  
 	def update_one new_day, new_open, new_close
-		@hours.each_pair do |day, times|
-			if day == new_day 
-				times[:open] = new_open 
-				times[:close] = new_close 
-			end
-		end 
-		hours
+		hours[new_day.to_sym][:open] = new_open
+		hours[new_day.to_sym][:close] = new_close
+	end 
+
+	def close day 
+		hours[day.to_sym][:opened] = false 
+	end 
+
+	def closed?
+		closed = []
+		hours.each_pair do |day, times|
+			closed << day.to_s if times[:opened] == false 
+		end
+		closed
 	end 
 
 
-	def calculate_deadline date, deadline
+	def calculate_deadline deadline, date
 		deadline_time = Time.parse(date) + (deadline)
-	end 
+		day = deadline_time.strftime("%a")
+		time = deadline_time.strftime("%I:%M") #24 hour
+		deadline_time.strftime("%a %b %d %I:%M:%S %Y")
+		
+	end
+
+	def army_time time
+		if time[-2..-1] == "PM" 
+			regex = /(.*):/
+			new_time = regex.match(time)[1].to_i + 12 
+		end 
+	end
+
+	def open? day, time 
+		open = army_time(hours[day.to_sym][:open])
+		close = army_time(hours[day.to_sym][:close])
+		week_array = 
+
+		if hours[day.to_sym][:opened] && open < time && close > time 
+			return true 
+		else 
+			hash.keys.index{||}
+		end 
+
+	end
+
 
 end
